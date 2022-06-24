e7d5b284cb7c44c3d99f3236fccb22c931336aff
NXord <noe.rolland03@gmail.Com>
2022-06-24 16:39:37 +0200

    fin de journer vendredi

---
+++ Assets/ScriptCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptCamera : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f;
+    float xRotation = 0f;
+
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -20f, 20f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        player.Rotate(Vector3.up * mouseX);
+    }
+}
